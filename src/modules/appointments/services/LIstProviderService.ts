5b058161bde3aab25bb3389589ab16e9a7bfaf5e
acpn <ant.pnasc@gmail.com>
2020-09-13 20:24:56 -0300

    Finishing coverage

---
+++ src/modules/appointments/services/LIstProviderService.ts
@@ -0,0 +1,32 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUserRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProviderService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+  ) { }
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id
+    });
+
+    if (!users) {
+      throw new AppError('Users not found');
+    }
+
+    return users;
+  }
+}
+
+export default ListProviderService;
